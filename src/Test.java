1a6fbed04746d5b4920a5f0ed8d4df5db9d83cf6
daniyuu <daniyuu@163.com>
2015-07-26 13:47:39 +0800

    test eclipse git commit

---
+++ src/Test.java
@@ -0,0 +1,30 @@
+/**
+ * 
+ */
+
+/**
+ * @author Daniyuu
+ *
+ */
+public class Test {
+
+	/**
+	 * 
+	 */
+	public Test() {
+		// TODO Auto-generated constructor stub
+		int[] scores = new int[4];
+		scores[2] = 1;
+		System.out.println(scores[2]);
+	}
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Test test = new Test();
+
+	}
+
+}
